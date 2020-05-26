0717c012ab3cb1836beb99ce4c7cfa91cc145328
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-27 00:36:16 +0430

    Edit
    file

--- src/Components/Bimito/Scrool.js
+++ src/Components/Bimito/Scrool.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import Plx from 'react-plx';
+// import React, { Component } from 'react';
+// import Plx from 'react-plx';
 
-class Example extends Component {
-  render() {
-    return (
-      <Plx
-        className='MyAwesomeParallax'
-        parallaxData={ ... } // your parallax effects, see beneath
-      >
-        /* Your content */
-      </Plx>
-    );
-  }
-}
+// class Example extends Component {
+//   render() {
+//     return (
+//       <Plx
+//         className='MyAwesomeParallax'
+//         parallax Data={ ... } // your parallax effects, see beneath
+//       >
+//         /* Your content */
+//       </Plx>
+//     );
+//   }
+// }
