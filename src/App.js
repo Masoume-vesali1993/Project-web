4b780ccbd9bdf751dadac2bb52fa0f11d3e9b4d0
Masoume <masoumevsl198@gmail.com>
2020-06-16 13:53:48 +0430

    add component link
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
-        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/Contact" component={Projects} />
      {/* <Home />
      <About />
      <Contact />
