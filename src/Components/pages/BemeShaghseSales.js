28b3d661c58a8c2627d5ff2e034606d6930c4bf1
Masoume <masoumevsl198@gmail.com>
2020-06-19 22:17:22 +0430

    add part new page
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/Components/pages/BemeShaghseSales.js
+++ src/Components/pages/BemeShaghseSales.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { Row, Col } from 'antd';
+import '../Bimito/styles.css';
+import Navbar from "../Navbar";
+import Footer from "../Bimito/Footer";
 
-function BemeShaghseSales() {
+export default class extends React.Component {
+constructor(props){
+    super(props);
+    this.state={
+        props : props
+    }
+}
+
+render(){
     return (
         <div>
-            <h1>استعلام بیمه شخص ثالث</h1>
+            <Navbar />
+                {this.props.children}
+                <div>
+                    <h1 className="title-text">استعلام بیمه شخص ثالث</h1>
+                    <Row>
+                        <Col span={12}>
+                        </Col>
+                        <Col span={12}>
+                            <h5>نوع، برند و سال ساخت خودروی خود را انتخاب کنید</h5>
+                        </Col>
+                    </Row>
+                </div>
+            <Footer />
         </div>
     );
 }
 
-export default BemeShaghseSales;
+}
