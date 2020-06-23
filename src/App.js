a7c5fc55ae1e59b96eb0d4320ed6635da7c19eb2
Masoume <masoumevsl198@gmail.com>
2020-06-23 19:21:29 +0430

    Edit Links
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import MainPart from './Components/Bimito/MainPart';
-// import MenuPart from './Components/Bimito/MenuPart';
+import MenuPart from './Components/Bimito/MenuPart';
 import Form from './Components/Bimito/Form'
 import Index from './Components/Bimito/Index';
 import Dargah from './Components/Bimito/Dargah';
@@ -13,23 +13,22 @@ import Bimito from './Components/Bimito/Biminto';
 import EndFooer from './Components/Bimito/EndFooer';
 import TopHeader from './Components/Bimito/TopHeader';
 import Scroll from './Components/Bimito/Scroll';
-// import { Route  } from 'react-router-dom';
-import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from './Components/Navbar';
-import BemeAtashSoze from './Components/pages/BemeAtashSoze';
-import BemeMosaferaty from './Components/pages/BemeMosaferaty';
-import BemeMasoliatPezeshgan from './Components/pages/BemeMasoliatPezeshgan';
-import BemeShaghseSales from './Components/pages/BemeShaghseSales';
-import BemeBadane from './Components/pages/BemeBadane';
-import BemeMotor from './Components/pages/BemeMotor';
-import Bemeomr from './Components/pages/Bemeomr';
-import BemeHavadeseAnfrade from './Components/pages/BemeHavadeseAnfrade';
-import DarmanTakmily from './Components/pages/DarmanTakmily';
-import BemeZelzele from './Components/pages/BemeZelzele';
-import Blog from './Components/pages/Blog';
-import ComponyBime from './Components/pages/ComponyBime';
-import LoginUsing from './Components/pages/LoginUsing';
-import SamaneBazaryabiBimito from './Components/pages/SamaneBazaryabiBimito';
+// import { Switch, Route, Redirect } from "react-router-dom";
+// import BemeAtashSoze from './Components/pages/BemeAtashSoze';
+// import BemeMosaferaty from './Components/pages/BemeMosaferaty';
+// import BemeMasoliatPezeshgan from './Components/pages/BemeMasoliatPezeshgan';
+// import BemeShaghseSales from './Components/pages/BemeShaghseSales';
+// import BemeBadane from './Components/pages/BemeBadane';
+// import BemeMotor from './Components/pages/BemeMotor';
+// import Bemeomr from './Components/pages/Bemeomr';
+// import BemeHavadeseAnfrade from './Components/pages/BemeHavadeseAnfrade';
+// import DarmanTakmily from './Components/pages/DarmanTakmily';
+// import BemeZelzele from './Components/pages/BemeZelzele';
+// import Blog from './Components/pages/Blog';
+// import ComponyBime from './Components/pages/ComponyBime';
+// import LoginUsing from './Components/pages/LoginUsing';
+// import SamaneBazaryabiBimito from './Components/pages/SamaneBazaryabiBimito';
 
 
 
@@ -45,9 +44,11 @@ class App extends Component {
 
           <Navbar />
 
+{/* 
           <Switch>
+
             <Route exact path="/BemeAtashSoze" component={BemeAtashSoze} />
-            {/* <Route exact path="/bemeAshkhas" component={BemeAshkhas} /> */}
+            <Route exact path="/bemeAshkhas" component={BemeAshkhas} />
             <Route exact path="/bemeMasoliatPezeshgan" component={BemeMasoliatPezeshgan} />
             <Route exact path="/bemeShaghseSales" component={BemeShaghseSales} />
             <Route exact path="/bemeBadane" component={BemeBadane} />
@@ -57,13 +58,13 @@ class App extends Component {
             <Route exact path="/BemeHavadeseAnfrade" component={BemeHavadeseAnfrade} />
             <Route exact path="/DarmanTakmily" component={DarmanTakmily} />
             <Route exact path="/BemeZelzele" component={BemeZelzele} />
-
             <Route exact path="/Blog" component={Blog} />
             <Route exact path="/ComponyBime" component={ComponyBime} />
             <Route exact path="/LoginUsing" component={LoginUsing} />
             <Route exact path="/SamaneBazaryabiBimito" component={SamaneBazaryabiBimito} />
-          </Switch>
-          
+            
+          </Switch> */}
+
           <MainPart />
 
           <Form/>
