12c28b27e667c6196156502dbcf7319e3acdf909
Masoume <masoumevsl198@gmail.com>
2020-07-17 21:08:20 +0430

    add code in component
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/Components/pages/SamaneBazaryabiBimito.js
+++ src/Components/pages/SamaneBazaryabiBimito.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Row, Col } from 'antd';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Input } from 'antd';
+
 
 function SamaneBazaryabiBimito() {
     return (
         <div>
             <Row>
-                <Col span={15}>
-                    <h2></h2>
+                <Col span={9}>
+                    <h6>برای ورود یا ثبت‌نام شماره تلفن همراه خود را وارد کنید</h6>
+                    <Input placeholder="ادامه" />
                 </Col>
-                <Col span={9}></Col>
+                <Col span={15}>
+                    <h2>!با بیمیتو پول دربیارید</h2>
+                    <br/>
+                    <h5>با ثبت نام به عنوان بازاریاب بیمیتو، به دیگران بیمه بفروشید
+                        و به ازای هر بیمه از بیمیتو پورسانت بگیرید</h5>
+                </Col>
             </Row>
         </div>
     );
 }
 
 export default SamaneBazaryabiBimito;
+
